dea43888f861007b82af7b16273d3a62a17760ca
eidytrinidad <eidyjtf@gmail.com>
2020-08-14 01:26:54 -0400

    adding and programming burger menu

---
+++ js/app.js
@@ -0,0 +1,24 @@
+const btnBurger = document.querySelector(".btnBurger");
+const icon = document.querySelector(".btnBurger i");
+const burgerMenu = document.querySelector(".burgerMenu");
+
+let value = false;
+btnBurger.addEventListener("click",()=>{
+
+    console.log(!value)
+    if (!value) {
+        value=true
+        burgerMenu.style.display="flex";
+        icon.classList.add("fa-times")
+     ;
+    }
+    else{
+        value=false;
+        burgerMenu.style.display="none";
+        icon.classList.remove("fa-times")
+    }
+
+
+
+    
+})
